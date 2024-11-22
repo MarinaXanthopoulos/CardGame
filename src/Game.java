21bcc179177e5b60f128192383205919503c4989
marina <marina.xanthopoulos@menloschool.org>
2024-11-22 11:30:55 -0800

    worked on game class, continuing game set up and trying to deal cards to player and computer (still working on this part)

--- src/Game.java
+++ src/Game.java
@@ -8,8 +8,9 @@ public class Game {
     public Game(Deck deck, Player person, Player computer) {
         // Declare deck attributes and make a deck
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        // All cards have their numerical value except a = 0 and k = -1
         int[] values = {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, -1};
-        String[] suits = {"hears", "spades", "clubs", "diamonds"};
+        String[] suits = {"hearts", "spades", "clubs", "diamonds"};
         deck = new Deck(ranks, suits, values);
 
         // Ask player for name
@@ -21,6 +22,9 @@ public class Game {
         person = new Player(answer);
         // Make computer
         computer = new Player("computer");
+
+        // Deal 4 cards to the player and computer
+        person.addCard();
     }
 
     public static void printInstructions(){
